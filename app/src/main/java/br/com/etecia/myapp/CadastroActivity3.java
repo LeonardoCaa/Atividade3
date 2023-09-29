8b7bca9e6ae3d1cc7776934edef0fc9860cce529
LeonardoCaa <lleonardocamargost@gmail.com>
2023-09-28 22:13:55 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/CadastroActivity3.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_cadastro3);
+    }
+}
